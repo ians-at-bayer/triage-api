3880d2a54ff99df97bf500915cfba294a342fad2
icsmit1 <ian.smith@bayer.com>
2021-11-17 13:36:44 -0600

    Remove the hook settings from Slack for all users

--- database/schema-changes.sql
+++ database/schema-changes.sql
@@ -12,3 +12,9 @@
 alter table people rename column slack_id to user_id;
 alter table settings rename column slack_hook_url to hook_url;
 alter table settings rename column slack_message to message;
+
+update settings set hook_url = 'http://teamshookurlgoeshere/';
+update settings set message = 'Hi I''m [name] and I''m on support this week. ' ||
+                              'If you want to keep up with who is on support, ' ||
+                              'try using our [contact card]([cardurl]). ' ||
+                              'It always has the latest support info for our team.';
